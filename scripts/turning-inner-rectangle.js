de6687253d5527c9a015f6792353c3e92f645612
ArtInLines <linesinart@gmail.com>
2022-03-31 00:59:50 +0200

    Restructured all methods, mainly to reduce use of global variables in functions

--- scripts/turning-inner-rectangle.js
+++ scripts/turning-inner-rectangle.js
@@ -2,16 +2,20 @@ const rootEl = document.getElementById('input-container');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+///////////
+// Classes
+
 class VectorPrimitive {
+	/** @param {...Number} components */
 	constructor(...components) {
 		this.components = components;
 	}
 
 	get x() {
-		return this.components[0] || null;
+		return this.components.length > 0 ? this.components[0] : null;
 	}
 	get y() {
-		return this.components[1] || null;
+		return this.components.length > 1 ? this.components[1] : null;
 	}
 	get dim() {
 		return this.components.length;
@@ -32,17 +36,22 @@ class VectorPrimitive {
 			this.components = arr;
 		}
 	}
+
+	getComponent(num) {
+		return this.components[num];
+	}
 }
 
 class Point extends VectorPrimitive {
+	/** @param {...Number} components */
 	constructor(...components) {
 		super(components);
 	}
 	toVector() {
-		return new Vector(...this.coordinate);
+		return new Vector(...this.components);
 	}
 	copy() {
-		return new Point(...this.coordinate);
+		return new Point(...this.components);
 	}
 }
 
@@ -52,12 +61,10 @@ class Vector extends VectorPrimitive {
 		super(components);
 	}
 
-	getComponent(num) {
-		return this.components[num];
-	}
-
 	toPoint() {
-		return new Point(...this.components);
+		let p = new Point(...this.components);
+		console.log(...this.components);
+		return p;
 	}
 
 	copy() {
@@ -127,52 +134,12 @@ class Vector extends VectorPrimitive {
 	 * @returns {Vector}
 	 */
 	static PointToPoint(A, B) {
-		return new Vector(...A.coordinate.map((a, i) => a - B.coordinate[i]));
+		return new Vector(...A.components.map((a, i) => a - B.components[i]));
 	}
 }
 
-ctx.lineWidth = 1;
-ctx.strokeStyle = '#000';
-
-let animationOpts = { interval: 10, radiusIncreasePerFrame: 0.5 };
-let animationID = null;
-let height = ctx.canvas.height;
-let width = ctx.canvas.height;
-let topCorner = new Point(0, 0);
-
-// function createInputEl({ label, id = label, value, onChange = null, changeInputEl = null, classPrefix = "input"}) {
-// 	const containerEl = document.createElement('div');
-// 	const labelEl = document.createElement('label');
-// 	const sliderEl = document.createElement('input');
-// 	const valEl = document.createElement('p');
-
-// 	containerEl.classList.add('slider-container');
-// 	containerEl.insertAdjacentElement('beforeend', labelEl);
-// 	containerEl.insertAdjacentElement('beforeend', sliderEl);
-// 	containerEl.insertAdjacentElement('beforeend', valEl);
-
-// 	labelEl.setAttribute('for', label);
-// 	labelEl.textContent = label;
-// 	labelEl.classList.add('label');
-// 	labelEl.classList.add('slider-label');
-
-// 	sliderEl.name = label;
-// 	sliderEl.type = 'range';
-// 	sliderEl.min = min;
-// 	sliderEl.max = max;
-// 	sliderEl.step = step;
-// 	sliderEl.classList.add('slider');
-
-// 	sliderEl.addEventListener('change', () => {
-// 		valEl.textContent = sliderEl.value;
-// 		if (typeof onChange == 'function') onChange(sliderEl.value, sliderEl, containerEl);
-// 	});
-
-// 	valEl.textContent = value;
-// 	valEl.classList.add('slider-val');
-
-// 	return containerEl;
-// }
+///////////
+// Methods
 
 function createSlider({ label, id = label, value = 50, min = 1, max = 100, step = 1, onChange = null, parent = rootEl }) {
 	const containerEl = document.createElement('div');
@@ -180,17 +147,18 @@ function createSlider({ label, id = label, value = 50, min = 1, max = 100, step 
 	const sliderEl = document.createElement('input');
 	const valEl = document.createElement('input');
 
-	labelEl.setAttribute('for', label);
+	labelEl.setAttribute('for', id);
+	labelEl.setAttribute('name', id + '-label');
 	labelEl.textContent = label;
 	labelEl.classList.add('label');
 	labelEl.classList.add('slider-label');
 
-	valEl.name = label + '-text';
+	valEl.name = id + '-text';
 	valEl.type = 'text';
 	valEl.value = value;
 	valEl.classList.add('slider-value-text');
 
-	sliderEl.name = label;
+	sliderEl.name = id;
 	sliderEl.type = 'range';
 	sliderEl.min = min;
 	sliderEl.max = max;
@@ -200,14 +168,12 @@ function createSlider({ label, id = label, value = 50, min = 1, max = 100, step 
 
 	sliderEl.addEventListener('change', () => {
 		valEl.value = sliderEl.value;
-		sliderEl.value = Number(sliderEl.value);
-		if (typeof onChange == 'function') onChange(sliderEl.value, sliderEl, containerEl);
+		if (typeof onChange == 'function') onChange(Number(sliderEl.value), sliderEl, containerEl);
 	});
 
 	valEl.addEventListener('change', () => {
 		sliderEl.value = valEl.value;
-		sliderEl.value = Number(sliderEl.value);
-		if (typeof onChange == 'function') onChange(sliderEl.value, sliderEl, containerEl);
+		if (typeof onChange == 'function') onChange(Number(sliderEl.value), sliderEl, containerEl);
 	});
 
 	containerEl.classList.add('slider-container');
@@ -221,36 +187,80 @@ function angleToRadians(angle) {
 	return (angle * Math.PI) / 180;
 }
 
+function radiansToAngle(radians) {
+	return (radians * 180) / Math.PI;
+}
+
 function f(length, radians) {
 	return Math.sqrt((Math.sin(radians) ** 2 * length ** 2) / (1 - Math.sin(radians) ** 2));
 }
 
-function animate({ interval = 10, radiusIncreasePerFrame = 0.5 }) {
-	console.log({ interval, radiusIncreasePerFrame });
-	let alpha = 0,
-		alphaRadians,
-		horizontalDistance,
-		verticalDistance;
+/**
+ * Returns a Point on the line between A and B based on the size of the angle
+ * @param {Point} A
+ * @param {Point} B
+ * @param {Number} angle
+ * @returns {Point}
+ */
+function getPointOnLine(A, B, angle, denominator = 45) {
+	const AB = Vector.PointToPoint(A, B);
+	console.log(A, B, AB);
+	AB.scale(angle % denominator);
+	return AB.toPoint();
+}
+
+/**
+ * Animate a rectangle based on its points, rather than its lengths, allowing to draw rotated rectangles
+ * @param {Point} A
+ * @param {Point} B
+ * @param {Point} C
+ * @param {Point} D
+ * @param {CanvasRenderingContext2D} context
+ */
+function drawRect(A, B, C, D, context = ctx) {
+	context.beginPath();
+	context.moveTo(A.x, A.y);
+	context.lineTo(B.x, B.y);
+	context.lineTo(C.x, C.y);
+	context.lineTo(D.x, D.y);
+	context.lineTo(A.x, A.y);
+	context.closePath();
+
+	context.stroke();
+}
+
+function animate({ topCorner, outerWidth, outerHeight, interval, angleIncreasePerFrame, innerRectAmount, context = ctx }) {
+	let alpha = 0;
+	const lastRect = { A: null, B: null, C: null, D: null };
+	const currentRect = { A: null, B: null, C: null, D: null };
 
 	return setInterval(() => {
-		alpha = (alpha + radiusIncreasePerFrame) % 45;
-		alphaRadians = angleToRadians(alpha);
+		alpha += angleIncreasePerFrame;
+		alpha = alpha % 45;
 
-		horizontalDistance = f(width, alphaRadians);
-		verticalDistance = f(height, alphaRadians);
+		context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear whole canvas
+		context.strokeRect(topCorner.x, topCorner.y, outerWidth, outerHeight); // Draw outer Rectangle
 
-		// Clear Canvas & draw outer rectangle
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		ctx.strokeRect(topCorner.x, topCorner.y, width, height);
+		lastRect.A = topCorner.copy();
+		lastRect.B = new Point(topCorner.x + outerWidth, topCorner.y);
+		lastRect.C = new Point(topCorner.x + outerWidth, topCorner.y + outerHeight);
+		lastRect.D = new Point(topCorner.x, topCorner.y + outerHeight);
 
-		ctx.moveTo(topCorner.x + horizontalDistance, topCorner.y);
-		ctx.beginPath();
-		ctx.lineTo(topCorner.x + width, topCorner.y + verticalDistance);
-		ctx.lineTo(topCorner.x + width - horizontalDistance, topCorner.y + height);
-		ctx.lineTo(topCorner.x, topCorner.y + height - verticalDistance);
-		ctx.lineTo(topCorner.x + horizontalDistance, topCorner.y);
-		ctx.closePath();
-		ctx.stroke();
+		console.log({ lastRect, topCorner });
+
+		for (let i = 0; i < innerRectAmount; i++) {
+			currentRect.A = getPointOnLine(lastRect.A, lastRect.B, alpha, 45);
+			currentRect.B = getPointOnLine(lastRect.B, lastRect.C, alpha, 45);
+			currentRect.C = getPointOnLine(lastRect.B, lastRect.D, alpha, 45);
+			currentRect.D = getPointOnLine(lastRect.D, lastRect.A, alpha, 45);
+
+			drawRect(currentRect.A, currentRect.B, currentRect.C, currentRect.D, context);
+
+			lastRect.A = currentRect.A;
+			lastRect.B = currentRect.B;
+			lastRect.C = currentRect.C;
+			lastRect.D = currentRect.D;
+		}
 	}, interval);
 }
 
@@ -262,52 +272,72 @@ function FPSToInterval(fps) {
 	return 1000 / fps;
 }
 
-/**
- * @param {Object} o
- * @param {Number} o.x Only decimal numbers
- * @param {Number} o.y Only decimal numbers
- */
-function resize(widthFactor = null, heightFactor = null) {
+function resize({ width, height, animID, topCorner, interval, angleIncreasePerFrame, innerRectAmount, widthFactor = 1, heightFactor = 1, context = ctx }) {
+	console.log('Resizing');
 	if (widthFactor) {
-		width = ctx.canvas.width * widthFactor;
-		topCorner.x = ctx.canvas.width / 2 - width / 2;
+		width = context.canvas.width * widthFactor;
+		topCorner.x = context.canvas.width / 2 - width / 2;
 	}
 	if (heightFactor) {
-		height = ctx.canvas.height * heightFactor;
-		topCorner.y = ctx.canvas.height / 2 - height / 2;
+		height = context.canvas.height * heightFactor;
+		topCorner.y = context.canvas.height / 2 - height / 2;
 	}
 
-	if (animationID) clearInterval(animationID);
-	animationID = animate(animationOpts);
+	if (animID) clearInterval(animID);
+	return animate({
+		topCorner,
+		outerHeight: height,
+		outerWidth: width,
+		interval,
+		angleIncreasePerFrame,
+		innerRectAmount,
+		context,
+	});
 }
 
-window.addEventListener('resize', resize);
-resize(1, 1);
+///////////
+// Global Variables & DOM Manipulation
 
-createSlider({ label: 'Width:', value: width, min: 1, max: ctx.canvas.width, onChange: (v) => resize(v / ctx.canvas.width) });
-createSlider({ label: 'Height:', value: height, min: 1, max: ctx.canvas.height, onChange: (v) => resize(null, v / ctx.canvas.height) });
+ctx.lineWidth = 1;
+ctx.strokeStyle = '#000';
+
+const opts = { topCorner: new Point(0, 0), width: ctx.canvas.width, height: ctx.canvas.height, animID: null, interval: 10000, angleIncreasePerFrame: 0.5, innerRectAmount: 1, context: ctx };
+
+window.addEventListener('resize', () => (animationID = resize(opts)));
+animationID = resize(opts);
+
+createSlider({ label: 'Width:', value: opts.width, min: 1, max: ctx.canvas.width, onChange: (v) => resize({ ...opts, widthFactor: v / 100 }) });
+createSlider({ label: 'Height:', value: opts.height, min: 1, max: ctx.canvas.height, onChange: (v) => resize({ ...opts, heightFactor: v / 100 }) });
 createSlider({ label: 'Line Width:', value: ctx.lineWidth, min: 1, max: 10, onChange: (v) => (ctx.lineWidth = v) });
 createSlider({
 	label: 'Angle Increase per Frame:',
-	value: animationOpts.radiusIncreasePerFrame,
+	value: opts.angleIncreasePerFrame,
 	min: 0.1,
 	max: 10,
 	step: 0.1,
 	onChange: (v) => {
-		animationOpts.radiusIncreasePerFrame = Number(v);
-		resize();
+		opts.angleIncreasePerFrame = v;
+		resize(opts);
 	},
 });
 createSlider({
 	label: 'Interval Length (ms):',
-	value: animationOpts.interval,
+	value: opts.interval,
 	min: 1,
 	max: 300,
 	step: 1,
 	onChange: (v) => {
-		animationOpts.interval = Number(v);
-		resize();
+		opts.interval = v;
+		resize(opts);
 	},
 });
-
-// animationID = animate(animationOpts);
+createSlider({
+	label: 'Inner Rectangles Amount',
+	value: opts.innerRectAmount,
+	min: 1,
+	max: 15,
+	onChange: (v) => {
+		opts.innerRectAmount = v;
+		resize(opts);
+	},
+});
